c726075c6e80c849858a9bcc1e06c6409e5463f1
Abhijana <abhijanaramanda@gmail.com>
2024-10-07 16:43:29 +0800

    docs(enums): :memo: doc for payment status enum

--- src/enums/payment-status.enum.ts
+++ src/enums/payment-status.enum.ts
@@ -1,4 +1,12 @@
-// Add docs why it's success only
+/**
+ * Represents the possible states of a payment.
+ *
+ * @remarks
+ * Currently, the only available status is `Success` because all payments are
+ * approved by default when the payment data is created manually. This is a
+ * temporary state until payment gateway integration is implemented, at which point
+ * more status options will be added to reflect the different payment outcomes.
+ */
 export enum PaymentStatus {
   Success = "success",
 }
